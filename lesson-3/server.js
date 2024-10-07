4f1c56b8bafa6ddfaf00e769e871bee338810ccf
eliyasmd50 <eliyasmohamed50@gmail.com>
2024-10-07 08:52:21 +0530

    webserver

--- lesson-3/server.js
+++ lesson-3/server.js
@@ -1,15 +1,103 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
+
 const logEvents = require('./logEvents');
-
 const EventEmitter = require('events');
-
 class MyEmitter extends EventEmitter {};
 
 //initialize objects
 const myEmitter = new MyEmitter();
 
-myEmitter.on('log', (msg) => logEvents(msg));
+// myEmitter.on('log', (msg) => logEvents(msg));
+// myEmitter.emit('log', 'Logs Emitted Successfully');
 
-setTimeout(() => {
-  //emit event
-  myEmitter.emit('log', 'Logs Emitted Successfully');
-}, 2000);
+
+const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath, 
+      !contentType.includes('image') ? 'utf8': ''
+    );
+    const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+    response.writeHead(200, {'Content-type': contentType});
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data
+    );
+  } catch (err) {
+    console.log(err);
+    response.statusCode = 500;
+    response.end();
+  }
+}
+
+const server = http.createServer((req, res) => {
+   console.log(req.url, req.method)
+
+   const extension = path.extname(req.url);
+
+   let contentType;
+
+   switch (extension) {
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.js':
+        contentType = 'text/javascript';
+        break;
+      case '.json':
+        contentType = 'application/json';
+        break;
+      case '.jpg':
+        contentType = 'image/jpeg';
+        break;
+      case '.png':
+        contentType = 'image/png';
+        break;
+      case '.txt':
+        contentType = 'text/plain';
+        break;
+      default:
+        contentType = 'text/html';
+        break;
+   }
+
+   let filePath =
+        contentType === 'text/html' && req.url ==='/'
+          ? path.join(__dirname, 'views', 'index.html' )
+          : contentType ==='text/html' && req.url.slice(-1) === '/'
+            ? path.join(__dirname, 'views', req.url, 'index.html')
+            : contentType ==='text/html'
+              ? path.join(__dirname, 'views', req.url)
+              : path.join(__dirname, req.url);
+
+   if(!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+   let fileExists = fs.existsSync(filePath);
+
+   if(fileExists) {
+    serveFile(filePath, contentType, res);
+   } else {
+    switch(path.parse(filePath).base){
+      case 'old-page.html':
+        res.writeHead(301, {'Location': '/new-page.html'})
+        res.end();
+        break;
+      case 'www-page.html':
+        res.writeHead(301, {'Location': '/'})
+        res.end();
+        break;
+      default:
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        break;
+    }
+   }
+
+  });
+
+server.listen(PORT, () => {
+  console.log(`Server is running on the port ${PORT}`);
+})
