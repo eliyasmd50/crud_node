2e83b22b36fda92ac4f1935a1751130768aa7b1c
eliyasmd50 <eliyasmohamed50@gmail.com>
2024-10-15 09:32:34 +0530

    changes done for authentication

---
+++ authentication/controllers/authController.js
@@ -0,0 +1,20 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data}
+}
+const bcrypt = require('bcrypt');
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd ) return res.status(400).json({"message": "username or password is required"});
+    const foundUser = usersDB.users.find(person => person.username === user);
+    if(!foundUser) return res.sendStatus(401); //unauthorized
+    const match = await bcrypt.compare(pwd, foundUser.password);
+    if (match) {
+        res.json({"success": `User ${user} is logged in!`});
+    } else {
+        res.sendStatus(401);//unauthorized
+    }
+}
+
+module.exports = {handleLogin};
--- authentication/controllers/registerController.js
+++ authentication/controllers/registerController.js
@@ -1,36 +1,32 @@
 const usersDB = {
     users: require('../model/users.json'),
-    setUsers: function(data) { this.users = data }
+    setUsers: function (data) { this.users = data }
 }
-
 const fsPromises = require('fs').promises;
 const path = require('path');
 const bcrypt = require('bcrypt');
 
-exports.handleNewUser = async (req, res) => {
+const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
-    if (!user || !pwd) {
-        return res.status(400).json({"message": "username or password is required"});
-    }
-    const duplicate = usersDB.users.find(person => person.username == user);
-    if (duplicate) {
-        return res.sendStatus(409); //conflict
-    }
-    console.log("set headers1");
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+    // check for duplicate usernames in the db
+    const duplicate = usersDB.users.find(person => person.username === user);
+    if (duplicate) return res.sendStatus(409); //Conflict 
     try {
         //encrypt the password
-        const hashPwd = await bcrypt.hash(pwd, 10);
+        const hashedPwd = await bcrypt.hash(pwd, 10);
         //store the new user
-        const newUser = { "username": user, "password": hashPwd};
+        const newUser = { "username": user, "password": hashedPwd };
         usersDB.setUsers([...usersDB.users, newUser]);
-        console.log("set headers2");
         await fsPromises.writeFile(
-            path.join([__dirname, '..', 'model', 'users.json']),
+            path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(usersDB.users)
         );
         console.log(usersDB.users);
-        res.status(201).json({"message": `New User ${user} created Successfully`})
-    } catch(err) {
-        res.status(500).json({"message": err.message});
+        res.status(201).json({ 'success': `New user ${user} created!` });
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
     }
 }
+
+module.exports = { handleNewUser };
